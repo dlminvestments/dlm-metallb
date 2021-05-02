bb79f0d9a245a26c3ce25fa8bec9892e3be1316c
DLM <55211307+dlminvestments@users.noreply.github.com>
2021-05-02 15:01:22 -0400

    Update sk-learn.py

--- neural network/training sets/sk-learn.py
+++ neural network/training sets/sk-learn.py
@@ -1,9 +1,21 @@
->>> from sklearn.neural_network import MLPClassifier
->>> X = [[0., 0.], [1., 1.]]
->>> y = [0, 1]
->>> clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
+from sklearn.neural_network import MLPClassifier
+X = [[0., 0.], [1., 1.]]
+y = [0, 1]
+clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
 ...                     hidden_layer_sizes=(5, 2), random_state=1)
 ...
->>> clf.fit(X, y)
+clf.fit(X, y)
 MLPClassifier(alpha=1e-05, hidden_layer_sizes=(5, 2), random_state=1,
               solver='lbfgs')
+X = [[0., 0.], [1., 1.]]
+y = [[0, 1], [1, 1]]
+clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
+...                     hidden_layer_sizes=(15,), random_state=1)
+...
+clf.fit(X, y)
+MLPClassifier(alpha=1e-05, hidden_layer_sizes=(15,), random_state=1,
+              solver='lbfgs')
+clf.predict([[1., 2.]])
+array([[1, 1]])
+clf.predict([[0., 0.]])
+array([[0, 1]])
